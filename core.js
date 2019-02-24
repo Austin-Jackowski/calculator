f44b7058d81f5d1e7d6f3ec721b5606845d813fa
Austin Jackowski <ajackowski@gmail.com>
2019-02-24 13:42:19 -0500

    Adjust index button id's and create buttons object to hold button information

--- core.js
+++ core.js
@@ -4,6 +4,12 @@ Calculator webapp
 
 // VARIABLES
 
+const buttonList = document.querySelectorAll('button')
+const buttons = {}
+
+// For each button in buttonList populate buttons object
+buttonList.forEach((e) => { buttons[e.id] = e.value })
+
 // FUNCTIONS
 
 // EVENT LISTENERS
