7731cf988847f8327d0ae8b55264eee5ba5f2b70
Austin Jackowski <ajackowski@gmail.com>
2019-03-02 22:29:05 -0500

    Add primary screen functionality & button event listeners/selectors

--- core.js
+++ core.js
@@ -5,11 +5,47 @@ Calculator webapp
 // VARIABLES
 
 const buttonList = document.querySelectorAll('button')
-const buttons = {}
+const topScreen = document.querySelector('#top-half')
+const bottomScreen = document.querySelector('#bottom-half')
 
-// For each button in buttonList populate buttons object
-buttonList.forEach((button) => { buttons[button.id] = button.value })
+// EVENT LISTENERS
+
+buttonList.forEach((button) => button.addEventListener('click', e => {
+
+  if (e.target.value === '=') {
+    //some function that displays result
+    bottomScreen.textContent = 'result'
+    topScreen.textContent += ''
+  } else if (e.target.value === 'clear') {
+    bottomScreen.textContent = ''
+    topScreen.textContent = ''
+  } else {
+    bottomScreen.textContent += e.target.value
+    topScreen.textContent += e.target.value
+  }
+}))
 
 // FUNCTIONS
 
-// EVENT LISTENERS
+function addition(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+const operate = function(operator, a, b) {
+  if (operator === '+') return addition(a, b)
+  else if (operator === '-') return subtract(a, b)
+  else if (operator === '*') return multiply(a, b)
+  else if (operator === '/') return divide(a, b)
+}
